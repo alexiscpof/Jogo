cb3f772c53e2bab4447905d54b8b478787127dec
Alexis Cristian Pertile de Oliveira Filho <alexis.filho@dcomp.ufs.br>
2025-08-24 18:42:48 -0300

    Transformei toda a movimentação do jogador, deixando-a mais funcional

--- Jogo nada funcional/script.js
+++ Jogo nada funcional/script.js
@@ -1,39 +1,58 @@
 document.addEventListener('DOMContentLoaded', () => {
     const quadradinhos = document.querySelectorAll(".grid div")
     const telaDePontuacao = document.querySelector("#pontuacao")
-    const largura = 16
-const estadoInicial = {
-        posicaoProtagonista: 361,
-        inimigos: [
-            0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-            16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
-            32, 33, 34, 35, 36, 37, 38, 39, 40, 41
-        ],
-        direcaoInimigos: 1,
-        pontuacao: 0,
-        inimigosDerrotados: [],
-        fimDeJogo: false,
-        vitoria: false, 
-    };
+    const inimigos = [
+         0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+         16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
+        32, 33, 34, 35, 36, 37, 38, 39, 40, 41
+    ]
+    //Cria o estado inicial do jogo, definindo a posição inicial do jogador e a largura da grade. Um freeze é usado para garantir a imutabilidade.
+    const estadoInicial = Object.freeze({ largura: 16, jogador: 352})
+    let direcao = 1
+    let pontuacao = 0
+    let inimigosDerrotados = []
+    inimigos.map((inimigos) => quadradinhos[inimigos].classList.add("inimigo"))
+    
 
-    let estadoAtual = estadoInicial;
-
-    inimigos.map(inimigo => quadradinhos[inimigo].classList.add('inimigo'))
-    quadradinhos[posicaoProtagonista].classList.add('protagonista')
-
-    function moverProtagonista(e) {
-        quadradinhos[posicaoProtagonista].classList.remove('protagonista')
-        switch(e.keyCode) {
-            case 37:
-                if (posicaoProtagonista % largura !== 0) posicaoProtagonista -=1
-                break
-            case 39:
-                if (posicaoProtagonista % largura < largura -1) posicaoProtagonista +=1
-                break
-        }
-        quadradinhos[posicaoProtagonista].classList.add('protagonista')
+    /*A função para mover o jogador utiliza o último estado do jogador e uma tecla pressionada. Caso a tecla pressionada seja a seta para a esquerda e o jogador não esteja
+    encostado na borda esquerda (é isso que a condição jogador%largura !== 0 significa), a função retorna um novo estado com a posição do jogador decrementada. Caso a
+    tecla pressionada seja a seta para direita e o jogador não esteja encostado na borda direita (é isso que a condição jogador%largura!==largura-1 significa), a função
+    retorna um novo estado com a posição do jogador incrementada. Caso nenhuma das condições seja satisfeita, a função retorna o mesmo estado recebido.*/
+    const moverJogador = (estado, tecla) => {
+        const { largura, jogador } = estado
+        if (tecla === "ArrowLeft" && jogador % largura !== 0) return { ...estado, jogador: jogador - 1 }
+        else if (tecla === "ArrowRight" && jogador % largura !== largura - 1) return { ...estado, jogador: jogador + 1 }
+        else return estado
     }
-    document.addEventListener('keydown', moverProtagonista)
+
+    /*A função renderizar atualiza o que é exibido na tela, ela recebe o estado anterior e o novo estado. Caso sejam diferentes, ela tira o trem da posição anterior e
+    o adiciona na nova posição.*/
+    const renderizar = (estadoAnterior, estadoAtual) => {
+        if (estadoAnterior !== estadoAtual) {
+        quadradinhos[estadoAnterior.jogador].classList.remove("jogador")
+        quadradinhos[estadoAtual.jogador].classList.add("jogador")
+        }
+    }
+
+    //A função loop recebe o estado atual do jogador e utiliza recursão e closure para atualizar e renderizar o estado em loop
+    const loop = (estado) => {
+        //Faz a primeira renderização do jogador, em seu estado inicial.
+        quadradinhos[estado.jogador].classList.add("jogador")
+        /*Função que calcula o próximo estado e é acionada sempre que um tecla é pressionada, por conta do addEventListener. O uso do closure é
+        importante para que a função tenha acesso ao argumento de loop(estado) e possa funcionar.*/
+        const calculaEstado = (evento) => {
+            //Chama as funções moverJogador e renderizar para calcular e renderizar o novo estado.
+            const novoEstado = moverJogador(estado, evento.key)
+            renderizar(estado, novoEstado)
+            //Exclui o próprio event listener.
+            document.removeEventListener("keydown", calculaEstado) 
+            //chama a função loop de forma recursiva.
+            loop(novoEstado) 
+        }
+        document.addEventListener("keydown", calculaEstado)
+    }
+
+    loop(estadoInicial)
 
     function moverInimigos() {
         const bordaEsquerda = inimigos[0] % largura === 0
@@ -90,8 +109,6 @@ const estadoInicial = {
                 pontuacao++
                 telaDePontuacao.textContent = pontuacao
             }
-            estado=atualizarPontuacao(estado,posicaoDoLaser)
-            render(estado,quadradinhos,pontuacao)
             if (posicaoDoLaser < largura) {
                 clearInterval(laserId)
                 setTimeout(() => quadradinhos[posicaoDoLaser].classList.remove('laser'), 100)
